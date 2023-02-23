6c4fc683b4152c734b75ff45c096b693906e4ef9
husam-shakeeb <husam_shakeeb@hotmail.com>
2023-02-23 23:13:03 +0100

    Updates 23-Feb-23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SrgAboutComponent } from './srg-about/srg-about.component';
+import { HomeComponent } from './home/home.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'about', component: SrgAboutComponent }
+  { path: 'about', component: SrgAboutComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'technologies', component: TechnologiesComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SrgNavbarComponent } from './srg-navbar/srg-navbar.component';
 import { SrgAboutComponent } from './srg-about/srg-about.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SrgNavbarComponent,
     SrgAboutComponent,
-    CarouselComponent
+    CarouselComponent,
+    HomeComponent,
+    TechnologiesComponent,
+    CatalogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
