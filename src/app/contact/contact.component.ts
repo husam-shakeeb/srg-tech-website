351ca24dac5ed3a7f6769cba2d8e3ef0ce9226be
husam-shakeeb <husam_shakeeb@hotmail.com>
2023-03-03 23:21:24 +0100

    before form validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,27 +9,39 @@ import * as emailjs from '@emailjs/browser'
 })
 export class ContactComponent {
   contactForm = new FormGroup({
-    subject: new FormControl('', Validators.required),
+    subject: new FormControl(''),
     name: new FormControl('', Validators.required),
-    email: new FormControl('email@form.com', [Validators.required, Validators.email]),
+    email: new FormControl('', Validators.email),
     company: new FormControl(''),
     message: new FormControl('', Validators.required),
-    phone: new FormControl('', Validators.pattern('[ +()0-9]+'))
+    phone: new FormControl('')
   })
+ 
 
-  logData() {
+  ngAfterViewInit() {
+    emailjs.init("user_JBaPN7atg1zx0DWAoXtIA")
+  }
+
+  sendEmail() {
     var templateParams = {
       user_subject: this.contactForm.controls.subject.value,
       user_name: this.contactForm.controls.name.value,
       user_phone: this.contactForm.controls.phone.value,
       user_message: this.contactForm.controls.message.value,
-    }
-    emailjs.init("user_JBaPN7atg1zx0DWAoXtIA")
+      user_company: this.contactForm.controls.company.value,
+    }  
+    
+    if (!this.contactForm.valid) {
+      alert('form invalid')
+      return
+    }   
+
+    
 
     emailjs.send('service_neeb9kq', 'template_ap8u45n', templateParams)
-      .then(function (response) {
+      .then(function (response: any) {
         console.log('SUCCESS!', response.status, response.text)
-      }, function (error) {
+      }, function (error: any) {
         console.log('FAILED...', error)
       })
   }
