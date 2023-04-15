2ee952266307cbd06ce40c340757e550c0278f3c
husam-shakeeb <husam_shakeeb@hotmail.com>
2023-04-15 17:42:37 +0200

    minor changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,74 +1,33 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms'
-import * as emailjs from '@emailjs/browser'
+
+import { EmailServiceService } from '../services/email-service.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
+  providers: [EmailServiceService]
 })
+
 export class ContactComponent {
+  constructor(private _email: EmailServiceService) { }
   contactForm = new FormGroup({
-    subject: new FormControl(''),
     name: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.email),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    phone: new FormControl(''),
     company: new FormControl(''),
-    message: new FormControl('', Validators.required),
-    phone: new FormControl('')
+    subject: new FormControl(''),       
+    message: new FormControl('', Validators.required)
+    
   })
- 
+
+  public get email() {
+    return this._email
+  }
+
+
   nameMessageText = ''
-  nameMessageStyle = {
-    'display': '',
-    'color': ''
-  }
-  nameBorderStyle = {
-    'border-color': '',
-    'box-shadow': '',
-  }
-  
-  ngAfterViewInit() {
-    emailjs.init("user_JBaPN7atg1zx0DWAoXtIA")
-
-    this.contactForm.controls.name.valueChanges.subscribe(value => {
-      this.nameMessageStyle.display = 'block'
-
-      if (!this.contactForm.controls.name.valid) {
-        this.nameBorderStyle['border-color'] = 'red'
-        this.nameBorderStyle['box-shadow'] = '0 0 0 0.2rem rgba(240,50,60,.25)'
-        this.nameMessageStyle.color = 'red'
-        this.nameMessageText = 'Not Valid'
-
-      } else {
-        this.nameBorderStyle['border-color'] = 'green'
-        this.nameBorderStyle['box-shadow'] = '0 0 0 0.2rem rgba(00,250,30,.25)'
-        this.nameMessageStyle.color = 'green'
-        this.nameMessageText = 'Valid'
-      }
-    })
-  }
-
-  sendEmail() {
-    var templateParams = {
-      user_subject: this.contactForm.controls.subject.value,
-      user_name: this.contactForm.controls.name.value,
-      user_phone: this.contactForm.controls.phone.value,
-      user_message: this.contactForm.controls.message.value,
-      user_company: this.contactForm.controls.company.value,
-    }  
-    
-    if (!this.contactForm.valid) {
-      alert('form invalid')
-      return
-    }   
-
-    
-
-    emailjs.send('service_neeb9kq', 'template_ap8u45n', templateParams)
-      .then(function (response: any) {
-        console.log('SUCCESS!', response.status, response.text)
-      }, function (error: any) {
-        console.log('FAILED...', error)
-      })
-  }
+  myMessageClass = ''
+  // myBorderClass = ''
 }
---
+++ src/app/services/email-service.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import * as emailjs from '@emailjs/browser'
+
+@Injectable ()
+export class EmailServiceService {
+
+  constructor() { 
+    emailjs.init("user_JBaPN7atg1zx0DWAoXtIA")
+  }
+  
+  sendEmail(contactForm: FormGroup) {
+    const templateParams = {
+      user_email: contactForm.controls['email'].value,
+      user_subject: contactForm.controls['subject'].value,
+      user_name: contactForm.controls['name'].value,
+      user_phone: contactForm.controls['phone'].value,
+      user_message: contactForm.controls['message'].value,
+      user_company: contactForm.controls['company'].value,
+    }  
+    
+    if (!contactForm.valid) {
+      alert('Please make sure all required fields are filled and try again')
+      return
+    }   
+
+    
+
+    emailjs.send('service_neeb9kq', 'template_ap8u45n', templateParams)
+      .then(function (response: any) {
+        console.log('SUCCESS!', response.status, response.text)
+      }, function (error: any) {
+        console.log('FAILED...', error)
+      })
+  }
+}
